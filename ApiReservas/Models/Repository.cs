3d2088010991e4c4e19473c411b933ac13e93c61
Lucas Silva <lucassilva996@outlook.com>
2021-07-22 01:40:51 -0300

    Reestrutura das pastas e criação da web api

---
+++ ApiReservas/Models/Repository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiReservas.Models
+{
+    public class Repository : IRepository
+    {
+        private Dictionary<int, Reserva> items;
+        public Reserva this[int id] => throw new NotImplementedException();
+
+        public Repository()
+        {
+            items = new Dictionary<int, Reserva>();
+
+            new List<Reserva>
+            {
+                new Reserva {ReservaId=1, Nome="Lucas", InicioLocacao = "Sao Paulo", FimLocacao = "Guarulhos" },
+                new Reserva {ReservaId=2, Nome="Dani", InicioLocacao = "Sao Paulo", FimLocacao = "Suzano" },
+                new Reserva {ReservaId=3, Nome="Tiago", InicioLocacao = "Sao Paulo", FimLocacao = "Itaquá" },
+            }.ForEach(r => AddReserva(r));
+        }
+
+        public IEnumerable<Reserva> Reservas => items.Values;
+
+        public Reserva AddReserva(Reserva reserva)
+        {
+            if(reserva.ReservaId == 0)
+            {
+                int key = items.Count;
+                while(items.ContainsKey(key)) { key++; };
+                reserva.ReservaId = key;
+            }
+            items[reserva.ReservaId] = reserva;
+            return reserva;
+        }
+
+        public void DeleteReserva(int id)
+        {
+            items.Remove(id);
+        }
+
+        public Reserva UpdateReserva(Reserva reserva)
+        {
+            AddReserva(reserva);
+            return reserva;
+        }
+    }
+}
---
+++ ApiReservas/Models/Reserva.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiReservas.Models
+{
+    public class Reserva
+    {
+        public int ReservaId { get; set; }
+        public string Nome { get; set; }
+        public string InicioLocacao { get; set; }
+        public string FimLocacao { get; set; }
+    }
+}
