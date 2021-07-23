1888816ecfb696e625c84c56fdfaea62f2a56a6c
Lucas Silva <lucassilva996@outlook.com>
2021-07-23 00:23:14 -0300

    Implementando get e put

--- mvc_reservas/Controllers/ReservasController.cs
+++ mvc_reservas/Controllers/ReservasController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace mvc_reservas.Controllers
@@ -16,17 +17,75 @@ namespace mvc_reservas.Controllers
         public async Task<IActionResult> Index()
         {
             List<Reserva> listaReservas = new List<Reserva>();
-
-            using (var httpClient = new HttpClient())
+            try
             {
-                using(var response = await httpClient.GetAsync(apiUrl))
+                using (var httpClient = new HttpClient())
                 {
-                    string apiResponse = await response.Content.ReadAsStringAsync();
-                    listaReservas = JsonConvert.DeserializeObject<List<Reserva>>(apiResponse);
+                    using (var response = await httpClient.GetAsync(apiUrl))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        listaReservas = JsonConvert.DeserializeObject<List<Reserva>>(apiResponse);
+                    }
                 }
+                return View(listaReservas);
             }
+            catch (Exception ex)
+            {
+                throw new ArgumentOutOfRangeException("Erro inesperado!", ex);
+            }
+            
+        }
 
-            return View(listaReservas);
+        public ViewResult GetReserva() => View();
+        [HttpPost]
+        public async Task <IActionResult> GetReserva(int id)
+        {
+            Reserva reserva = new Reserva();
+
+            try
+            {
+
+                using (var httpClient = new HttpClient())
+                {
+                    using(var response = await httpClient.GetAsync(apiUrl + "/" + id))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        reserva = JsonConvert.DeserializeObject<Reserva>(apiResponse);
+                    }
+                }
+                return View(reserva);
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentOutOfRangeException("Erro inesperado!", ex);
+            }
+        }
+
+        public ViewResult AddReserva() => View();
+        [HttpPost]
+        public async Task<IActionResult> AddReserva(Reserva reserva)
+        {
+            Reserva reservaRecebida = new Reserva();
+
+            try
+            {
+
+                using (var httpClient = new HttpClient())
+                {
+                    StringContent content = new StringContent(JsonConvert.SerializeObject(reserva),
+                        Encoding.UTF8, "application/json");
+                    using (var response = await httpClient.PostAsync(apiUrl, content))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        reservaRecebida = JsonConvert.DeserializeObject<Reserva>(apiResponse);
+                    }
+                }
+                return View(reservaRecebida);
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentOutOfRangeException("Erro inesperado!", ex);
+            }
         }
     }
 }
